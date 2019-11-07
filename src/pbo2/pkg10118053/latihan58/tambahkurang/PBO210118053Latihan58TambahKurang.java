b7167da4a07ca9ab733e9909cc31eb4d51274896
kevinalvian <55798459+kevinalvian@users.noreply.github.com>
2019-11-07 22:58:40 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118053/latihan58/tambahkurang/PBO210118053Latihan58TambahKurang.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118053.latihan58.tambahkurang;
+
+/**
+ *
+ * @author ACER
+ */
+public class PBO210118053Latihan58TambahKurang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         jumlahBilang hasil = new jumlahBilang();
+        hasil.tampilhasiljumalah();
+        selisiBilang hasil1 = new selisiBilang();
+        hasil1.tampilselisi();
+    }
+    
+}
---
+++ src/pbo2/pkg10118053/latihan58/tambahkurang/jumlahBilang.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118053.latihan58.tambahkurang;
+
+/**
+ *
+ * @author ACER
+ */
+public class jumlahBilang extends bilangan{
+    public void tampilhasiljumalah(){
+        System.out.println("hasil penjumlahan = "+(getX()+getY()));
+        
+    }
+    
+}
